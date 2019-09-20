0ec4f8530e36ecdb222e1ef1a2c7bc565b8896e7
Chris Quintin <53836990+QuintinChris@users.noreply.github.com>
2019-09-20 12:40:38 -0400

    Update scripts.js
    Add copyEmail() functionality that copies email to clipboard

--- src/scripts.js
+++ src/scripts.js
@@ -12,3 +12,12 @@ $(document).ready(function () {
         }
     });
 });
+
+function copyEmail(element) {
+    var $temp = $("<input>");
+    $("body").append($temp);
+    $temp.val($(element).text()).select();
+    document.execCommand("copy");
+    $temp.remove();
+    alert('Copied to clipboard!')
+}
